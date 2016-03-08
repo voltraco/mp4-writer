ad0331e5e872c169810602439680a773bad8ee03
James Halliday <mail@substack.net>
2016-03-07 22:30:13 -0800

    basic updating flow

--- example/write.js
+++ example/write.js
@@ -1,10 +1,28 @@
-var writer = require('../')
+var metaWriter = require('../')
 var fs = require('fs')
 var xtend = require('xtend')
+var minimist = require('minimist')
+var argv = minimist(process.argv.slice(2), {
+  alias: { o: 'outfile', i: 'infile' }
+})
 
-fs.createReadStream(process.argv[2])
-  .pipe(writer(function (err, meta, next) {
-    next(null, xtend(meta, {
-      ')nam': 'whatever'
+if (argv._[0] === 'show') {
+  fs.createReadStream(argv.infile)
+    .pipe(metaWriter(function (err, meta, next) {
+      console.log(meta)
     }))
-  }))
+} else if (argv._[0] === 'set') {
+  var updates = {}
+  argv._.slice(1).forEach(function (kv) {
+    var s = kv.split(':'), key = s[0], value = s[1]
+    updates[key] = value
+  })
+  fs.createReadStream(argv.infile)
+    .pipe(metaWriter(function (rec, next) {
+      if (updates.hasOwnProperty(rec.key)) {
+        rec.value = updates[rec.key]
+      }
+      return rec
+    }))
+    .pipe(fs.createWriteStream(argv.outfile))
+}
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var mp4 = require('mp4-stream')
 var fs = require('fs')
 var inspect = require('util').inspect
+var duplexify = require('duplexify')
+var concat = require('concat-stream')
 
 module.exports = function (cb) {
-  return readProp(['moov','udta','meta'], function (err, box) {
+  return walkProps(['moov','udta','meta'], function (err, box, next) {
     var d = mp4.decode()
-    var meta = {}
+    var e = mp4.encode()
     d.on('box', function (hbox) {
       d.decode(function (sbox) {
         var type = sbox.buffer.slice(4,8).toString()
@@ -18,35 +20,66 @@ module.exports = function (cb) {
           } else {
             value = sbox.buffer.slice(12 + 3 + len).toString()
           }
-          meta[key] = value
-        }
+          var newrec = cb({ key: key, value: value })
+          if (newrec) {
+            sbox.type = newrec.key
+            if (key === 'trkn') {
+              sbox.buffer.writeUInt32BE(12+len)
+            } else {
+              sbox.buffer = Buffer.concat([
+                sbox.buffer.slice(0, 12 + 3),
+                Buffer(String(newrec.value))
+              ])
+            }
+            e.box(sbox)
+          }
+        } else e.box(sbox)
       })
     })
-    var sbuf = box.buffer.slice(box.buffer.readUInt32BE(4) + 8 + 4)
+    var offset = box.buffer.readUInt32BE(4) + 8 + 4
+    var sbuf = box.buffer.slice(offset)
+    e.pipe(concat(function (buf) {
+      buf.copy(box.buffer, offset)
+      next(null, box)
+    }))
     d.end(sbuf)
-    cb(null, meta, function (err, nmeta) {
-      console.log('set meta', nmeta)
-    })
   })
 }
 
-function readProp (props, cb) {
+function walkProps (props, cb) {
   var d = mp4.decode()
+  var e = mp4.encode()
+  var stream = duplexify()
+  stream.setWritable(d)
   d.on('box', function (hbox) {
-    if (hbox.type === props[0]) {
-      d.decode(function (box) {
-        if (props.length === 1) cb(null, box)
-        else if (box.otherBoxes) {
-          for (var i = 0; i < box.otherBoxes.length; i++) {
-            var b = box.otherBoxes[i]
-            if (b.type === props[1]) {
-              return readProp(props.slice(2), cb).end(b.buffer)
-            }
+    d.decode(function (box) {
+      if (hbox.type !== props[0]) {
+        stream.setReadable(e)
+        e.box(box)
+      } else if (props.length === 1) {
+        stream.setReadable(e)
+        cb(null, box, function (err, newbox) {
+          if (err) return d.emit('error', err)
+          stream.setReadable(e)
+          if (newbox) e.box(newbox)
+        })
+      } else if (box.otherBoxes) {
+        for (var i = 0; i < box.otherBoxes.length; i++) {
+          var b = box.otherBoxes[i]
+          if (b.type === props[1]) {
+            var r = walkProps(props.slice(2), cb)
+            stream.setReadable(r)
+            r.end(b.buffer)
+            return
           }
-          cb(null, undefined)
-        } else cb(null, undefined)
-      })
-    } else d.ignore()
+        }
+        stream.setReadable(e)
+        e.box(box)
+      } else {
+        stream.setReadable(e)
+        e.box(box)
+      }
+    })
   })
-  return d
+  return stream
 }
